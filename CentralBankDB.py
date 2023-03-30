7401c87c3e28638c4a35020f7e431416de2a963d
manasvaidya <101902869+manasvaidya@users.noreply.github.com>
2023-03-30 14:13:33 +0530

    Update CentralBankDB.py

--- CentralBankDB.py
+++ CentralBankDB.py
@@ -7,18 +7,6 @@ import DatastreamDSWS as DSWS
 import streamlit as st
 
 
-
-
-
-##############################################
-######## Initialise Connection
-##############################################
-
-
-
-# Create connection using the username and password
-ds = DSWS.Datastream(username = username, password = password)
-
 st.write("DB username:", st.secrets["username"])
 st.write("DB password:", st.secrets["password"])
 
