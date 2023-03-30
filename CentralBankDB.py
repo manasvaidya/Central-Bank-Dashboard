3031534c95dc56cdee0a4f8a5bc48e51171da411
manasvaidya <101902869+manasvaidya@users.noreply.github.com>
2023-03-30 14:19:11 +0530

    Update CentralBankDB.py

--- CentralBankDB.py
+++ CentralBankDB.py
@@ -69,7 +69,106 @@ dict_rate = {"US": "Fed Funds",
 
 
 
+# Key value pairs for Country - DS Rate Code
+dict_rate_code = {"US": "USPRATE.", 
+                  "UK": "UKPRATE.", 
+                  "Canada":"CN14309",
+                  "Euro Area":"EURODEP",
+                  "Japan":"JPPRATE.",
+                  "Switzerland":"SWBCBPR",
+                    "Australia":"AUPRATE.",
+                    "New Zealand":"NZPRATE.",
+                    "Sweden":"SDPRATE.",
+                    "Norway":"NWPRATE.",
+                    "Denmark":"DKI60...",
+                    "Hong Kong":"HKBCBPR",
+                    "China":"CHLPR1YRR",
+                    "South Korea":"KOPRATE.",
+                    "Taiwan":"TWPRATE.",
+                    "Malaysia":"MYPRATE.",
+                    "Indonesia":"IDPRATE.",
+                    "Philippines":"pHPREPO.",
+                    "Thailand":"THPRATE.",
+                    "India":"INPRATE.",
+                    "Russia":"RSPRATE.",
+                    "Poland":"POPRATE",
+                    "Czech Republic":"CZPRATE.",
+                    "Saudi Arabia":"SIPRATE",
+                    "Turkey":"TKPRATE.",
+                    "South Africa":"SAPRATE.",
+                    "Nigeria":"NGPRATE.",
+                  "Mexico":"MXPRATE.",
+                    "Colombia":"CBPRATE.",
+                    "Chile":"CLPRATE.",
+                    "Peru":"PEPRATE.",
+                 "Brazil": "BRPRATE."}
 
-st.write(dict_rate)
+# Key value pairs for Country - DS Inflation Code
+dict_inf_code = {"US": "USCONPRCE", 
+                 "UK": "UKCONPRCF",
+                 "Canada": "CNCONPRCF",
+                 "Euro Area": "EMEBCPALE",
+                 "Japan": "JPCONPRCF",
+                 "Switzerland": "SWCONPRCF",
+                 "Australia": "AUCONPRCF",
+                 "New Zealand": "NZCONPRCF",
+                 "Sweden": "SDCONPRCF",
+                 "Norway": "NWCONPRCF",
+                 "Denmark": "DKCONPRCF",
+                 "Hong Kong": "HKCONPRCF",
+                 "China": "CHCCPI..E",
+                 "South Korea": "KOCONPRCF",
+                 "Taiwan": "TWCONPRCF",
+                 "Malaysia": "MYCONPRCF",
+                 "Indonesia": "IDCONPRCF",
+                 "Philippines": "PHCONPRCF",
+                 "Thailand": "THCONPRCF",
+                 "India": "INCONPRCF",
+                 "Russia": "RSCONPRCF",
+                 "Poland": "POCONPRCF",
+                 "Czech Republic": "CZCONPRCF",
+                 "Saudi Arabia": "SICONPRCF",
+                 "Turkey": "TKCONPRCF",
+                 "South Africa": "SACONPRCF",
+                 "Nigeria": "NGCONPRCF",
+                 "Mexico": "MXCONPRCF",
+                 "Colombia": "CBCONPRCF",
+                 "Chile": "CLCPCOREF",
+                 "Peru": "PECCPI..E",
+                "Brazil": "BRCONPRCF"}
+
+
+# Column names map
+dict_col = {'country': "Country", 
+            'central_bank_rate': "Instrument", 
+            'rate_code': "Policy Rate DS Code", 
+            'inflation_code': "CPI DS Code", 
+            'policy_rate': "Policy Rate (%)",
+            'inflation': "Inflation (%)", 
+            'real_rate': "Real Rate (%)",
+            'direction_of_last_move': "Last Move", 
+            'date_of_last_move': "Date of Last Move", 
+            'policy_rate_change_pp': "Change (%)",
+            'policy_rate_date': "Date of Policy Rate", 
+            'inflation_date': "Date of Inflation"
+           }
+
+
+
+
+###############################################################################
+####################### Parameter Initialisation and Preprocessing
+###############################################################################
+
+## Define countries to extract data for
+input_list = ["US", "UK", "Canada", "Euro Area", "Japan", "Switzerland", "Australia",
+             "New Zealand", "Sweden", "Norway", "Denmark", "Hong Kong", "China",
+              "South Korea", "Taiwan", "Malaysia", "Indonesia", "Philippines", 
+              "Thailand", "India", "Russia", "Poland", "Czech Republic", "Saudi Arabia",
+              "Turkey", "South Africa", "Nigeria", "Mexico", "Colombia", "Chile", "Peru"
+              ,"Brazil"
+             ]
+
+st.write(dict_rate_code)
 
 
