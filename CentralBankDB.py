9728e8095dbed0a5761746a3b855039f20b71c04
manasvaidya <101902869+manasvaidya@users.noreply.github.com>
2023-03-30 14:09:17 +0530

    Update CentralBankDB.py

--- CentralBankDB.py
+++ CentralBankDB.py
@@ -1,34 +1,67 @@
 ###########################################
 ########## Package Imports
 ###########################################
-import pandas as pd, numpy as np
+import pandas as pd
+import numpy as np
 import DatastreamDSWS as DSWS
-
-# from datetime import datetime as dt
-# import dataframe_image as dfi
-
 import streamlit as st
 
 
-##### Get Username ans passowrd
-# username = st.text_input("Enter Datastream username:")
-# password = st.text_input("Enter Datastream password:", type="password")
-
-username = st.text_input("Enter a username")
-password = st.text_input("Enter a password", type="password")
 
 
-# create a dataframe with a boolean column
-data = {'Name': ['John', 'Jane', 'Bob', 'Alice'],
-        'Age': [25, 30, 28, 22],
-        'Gender': ['M', 'F', 'M', 'F'],
-        'Salary': [50000, 70000, 60000, 55000]}
 
-# create dataframe from data
-df = pd.DataFrame(data)
+##############################################
+######## Initialise Connection
+##############################################
 
-# show the dataframe with checkboxes
-st.dataframe(df)
+
+
+# Create connection using the username and password
+ds = DSWS.Datastream(username = username, password = password)
+
+
+
+#########################################################################
+#################### Initialise Mappings
+#########################################################################
+# Key value pairs for Country - Policy Rate
+dict_rate = {"US": "Fed Funds",
+            "Canada": "Overnight Rate",
+            "Euro Area": "Main Refinancing Operations Rate",
+            "UK": "Bank Rate",
+            "Japan": "Overnight Call Rate",
+            "Switzerland": "Three-Month Libor", #"Policy Rate",
+            "Australia": "Cash Rate",
+            "New Zealand": "Official Cash Rate",
+            "Sweden": "Repo Rate",
+            "Norway": "Deposit Rate",
+            "Denmark": "Deposit rate", #"Discount Rate",
+            "Hong Kong": "Base Rate",
+            "China": "Prime Loan Rate",
+            "South Korea": "Base Rate",
+            "Taiwan": "Discount Rate",
+            "Malaysia": "Overnight Policy Rate",
+            "Indonesia": "Reverse Repo Rate",
+            "Philippines": "Policy Rate", #"Reverse Repo Rate",
+            "Thailand": "Overnight Repo Rate",
+            "India": "Repo Rate", #"Policy Repo Rate",
+            "Russia": "Key Rate",
+            "Poland": "Reference Rate",
+            "Czech Republic": "Key Interest Rate", #"Repo Rate",
+            "Saudi Arabia": "Repo Rate",
+            "Turkey": "One-Week Repo Rate", #"Repo Rate",
+            "South Africa": "Repo Rate",
+            "Nigeria": "Policy Rate",
+            "Brazil": "Selic Rate",
+            "Mexico": "Policy Rate", #"Target Rate",
+            "Colombia": "Policy Rate",
+            "Chile": "Policy Rate",
+            "Peru": "Monetary Policy Rate" #"Reference Rate"
+            }
+
+
+
+st.write(dict_rate)
 
 
 
